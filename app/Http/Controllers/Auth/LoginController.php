d78f477def3d6afecfc2d74d3635be21b034c6c8
kevincisnero01 <kevingabriel18@gmail.com>
2021-06-27 15:03:43 -0430

    view login | route login

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -12,4 +12,9 @@ class LoginController extends Controller
     {
         return view('login');
     }
+
+    public function login()
+    {
+        return request();
+    }
 }
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -1,4 +1,34 @@
 @extends('layouts.app')
 @section('content')
-	<p>hello super dev</p>
+<div class="row">
+	<div class="col-md-4 col-md-offset-4">
+		<div class="panel panel-default">
+			<div class="panel-heading">
+				Acceso a la Aplicación
+			</div>
+			<div class="panel-body">
+			
+			<form  method="POST" action="{{route('login')}}">
+				{{ csrf_field() }}
+				<div class="form-group">
+					<label for="email">Email</label>
+					<input class="form-control"
+						type="email" 
+						name="email" 
+					placeholder="Ingresa tu email">
+				</div>
+				<div class="form-group">	
+					<label for="email">Password</label>
+					<input class="form-control"
+						type="password" 
+						name="password" 
+						placeholder="Ingresa tu password">
+				</div>
+				<button class="btn btn-primary btn-block" type="submit">Acceder</button>
+			</form>
+
+			</div><!--panel-body-->
+		</div><!--panel-->
+	</div><!--col-->
+</div><!--row-->
 @endsection
