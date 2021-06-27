d3809d56b5507882c4eda6c864f5e3c1f97e6e90
kevincisnero01 <kevingabriel18@gmail.com>
2021-06-27 14:37:30 -0430

    setup inicial

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Custom Login</title>
+	<link rel="stylesheet" href="/css/app.css">
+</head>
+<body>
+	<div class="container">
+		@yield('content')
+	</div><!--container-->
+</body>
+</html>
---
+++ resources/views/login.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.app')
+@section('content')
+	<p>hello super dev</p>
+@endsection
