07b0bd7fa031c03f986a4d949cd30b43129735de
kevincisnero01 <kevingabriel18@gmail.com>
2021-06-27 15:07:29 -0430

    add bootstrap &jquery

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,11 +3,14 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Custom Login</title>
-	<link rel="stylesheet" href="/css/app.css">
+	<link rel="stylesheet" href="{{ asset('css/style.css') }}">
+    <link rel="stylesheet" href="{{ asset('plugins/bootstrap/css/bootstrap.css') }}">
 </head>
 <body>
 	<div class="container">
 		@yield('content')
 	</div><!--container-->
 </body>
+<script type="text/js" src="{{ asset('plugins/jquery/jquery.js') }}"></script>
+<script type="text/js" src="{{ asset('plugins/bootstrap/js/bootstrap.js') }}"></script>
 </html>
